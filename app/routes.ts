81c6a675eb2db48372344eff62019c9758c2c5f1
AzureAD\ManenoMgwami <Maneno.Mgwami@osiristrading.com>
2025-06-26 15:16:42 +0200

    removing admin

--- app/routes.ts
+++ app/routes.ts
@@ -2,12 +2,6 @@ import FilteredToursPage from "./components/FilteredToursPage";
 import Home from "./components/Home";
 import HomePage from "./components/HomePage";
 import Projects from "./components/Projects";
-import AddTour from "./components/admin/AddTour";
-import AdminHome from "./components/admin/AdminHome";
-import AdminTours from "./components/admin/AdminTours";
-import AdminWelcomePage from "./components/admin/AdminWelcomePage";
-import TourDashboard from "./components/admin/TourDashboard";
-import TourDetails from "./components/admin/TourDetails";
 
 const routes = [
   {
@@ -26,36 +20,6 @@ const routes = [
         path:"filteredtours",
         Component: FilteredToursPage
       }
-    ]
-  },
-  {
-    path: "/admin",
-    Component: AdminHome,
-    children: [
-      {
-        path:"",
-        Component:AdminWelcomePage
-      },
-      {
-        path:"tours",
-        Component: TourDashboard,
-        children:[
-          {
-            path:"",
-            Component:AdminTours,
-
-          },
-          {
-            path:"addtour",
-            Component:AddTour
-          },
-          {
-             path:"tourdetails",
-             Component:TourDetails
-          }
-        ]
-      }
-      
     ]
   }
 ];
